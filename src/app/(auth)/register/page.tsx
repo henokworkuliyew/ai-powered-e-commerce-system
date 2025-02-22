ba933b749f61cb028fb9b3dac2cf0981e3408ec7
hwgit <hwgojj@gmail.com>
2025-02-22 05:29:15 -0800

    initial commit

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import WrapForm from '@/components/UI/WrapForm'
+import React from 'react'
+import Register from './Register'
+
+const page = () => {
+  return (
+   <WrapForm>
+   <Register/>
+   </WrapForm>
+  )
+}
+
+export default page
