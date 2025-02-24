2fd44daa56515318333e0d4195c522685c1671ca
hwgit <hwgojj@gmail.com>
2025-02-24 04:15:54 -0800

    initial commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './Login'
+import WrapForm from '@/components/UI/WrapForm'
+
+const page = () => {
+  return (
+    <WrapForm>
+      <Login />
+    </WrapForm>
+  )
+}
+
+export default page
