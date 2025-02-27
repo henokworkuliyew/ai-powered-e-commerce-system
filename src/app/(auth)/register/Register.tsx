7e08ff5e4945a15374d3394057b9483672c3e810
hwgit <hwgojj@gmail.com>
2025-02-27 15:45:52 -0800

    added redirect to a certain page after authenticated

--- src/app/(auth)/register/Register.tsx
+++ src/app/(auth)/register/Register.tsx
@@ -6,20 +6,53 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import { AiOutlineGoogle } from 'react-icons/ai'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 const Register = () => {
-  const [isLoading , setIsLoadig] = useState(false)
-  const {register , handleSubmit ,formState:{errors}} = useForm<FieldValues>({
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: {
-      name:'',
-      email:'',
-      password:'',
-      phone:''
-    }
+      name: '',
+      email: '',
+      password: '',
+     
+    },
   })
-  const onSubmit:SubmitHandler<FieldValues> = (data) => {
-    setIsLoadig(true)
-    console.log(data)
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log('Submitting data:', data)
+    setIsLoading(true)
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        toast.success('register sucessfully!')
+        signIn('credentials', {
+          email: data.email,
+          password: data.password,
+          redirect: false, 
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push('/cart')
+            router.refresh()
+            toast.success('Successfully logged in')
+          }
+          if (callback?.error) {
+            toast.error('Invalid email or password to login')
+          }
+        })
+      })
+      .catch((error) => toast.error('Something went wrong!',error))
+      .finally(() => setIsLoading(false))
   }
+
   return (
     <div>
       <Heading text={'SignUp'} level={3} gradient />
@@ -39,7 +72,6 @@ const Register = () => {
         errors={errors}
         required
       />
-
       <Input
         id="password"
         label="Password"
@@ -49,14 +81,15 @@ const Register = () => {
         required
         type="password"
       />
-      <Input
+      {/* <Input
         id="phone"
         label="Phone"
         disabled={isLoading}
         register={register}
         errors={errors}
         required
-      />
+      /> */}
+
       <div className="mt-5 mb-5">
         <Button
           label={isLoading ? 'Loading' : 'Sign Up'}
@@ -70,6 +103,7 @@ const Register = () => {
           </Link>
         </span>
       </div>
+
       <Button
         label={isLoading ? 'Loading' : 'Sign Up with Google'}
         icon={AiOutlineGoogle}
