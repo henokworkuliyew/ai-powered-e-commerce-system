bdb4a64a8626045e64b2e700790f47713f2bdb0f
hwgit <hwgojj@gmail.com>
2025-02-27 15:46:55 -0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+
   images: {
     domains: ['images.unsplash.com', 'images.pexels.com'],
   },
   experimental: {
-    turbo: {
-      rules: {
-        '*.mdx': ['mdx-loader'],
-      },
-    },
+    turbo: false,
   },
 }
 
