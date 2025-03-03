581f387450a185ff438be309a49399f276d1f07e
hwgit <hwgojj@gmail.com>
2025-03-03 03:40:00 -0800

    exports authptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import bcrypt from 'bcrypt'
 import dbConnect from '@/lib/dbConnect'
 import User from '@/server/models/User' 
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -18,35 +18,25 @@ export default NextAuth({
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        
         await dbConnect()
-       console.log('Credentials:', credentials)
 
         if (!credentials?.email || !credentials?.password) {
-           
           throw new Error('Invalid email or password!')
         }
 
-        
         const user = await User.findOne({ email: credentials.email })
-        console.log('Incorrect password!')
         if (!user || !user.hashedPassword) {
           throw new Error('Invalid email or password!')
         }
 
-      
-           
         const isPasswordValid = await bcrypt.compare(
-          
           credentials.password,
           user.hashedPassword
         )
-console.log('Is password valid:', isPasswordValid)
         if (!isPasswordValid) {
           throw new Error('Invalid email or password!')
         }
 
-        
         return user
       },
     }),
@@ -57,4 +47,6 @@ console.log('Is password valid:', isPasswordValid)
     strategy: 'jwt',
   },
   debug: process.env.NODE_ENV === 'development',
-})
+}
+
+export default NextAuth(authOptions)
