76ad060f8021a2fa1426ecec5396b5fb3aff2838
hwgit <hwgojj@gmail.com>
2025-04-11 15:45:57 -0700

    change product type structure

---
+++ src/type/Product.ts
@@ -0,0 +1,24 @@
+export type Product = {
+  _id: string
+  id: string
+  name: string
+  description: string
+  category: {
+    name: string
+    subCategories: string[]
+  }
+  brand: string
+  images: {
+    color: string
+    colorCode: string
+    views: {
+      front: string
+      side: string
+      back: string
+    }
+  }[]
+  inStock: boolean
+  quantity: number
+  rating: number
+  price: number
+}
