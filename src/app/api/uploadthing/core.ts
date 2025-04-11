acb453ae993e989f53412f69e48f0c84d420fcf5
hwgit <hwgojj@gmail.com>
2025-04-11 15:07:28 -0700

    initial commit

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,36 @@
+
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
+import { UploadThingError } from 'uploadthing/server'
+
+const f = createUploadthing()
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const auth = (req: Request) => ({ id: "fakeId" });
+/* eslint-enable @typescript-eslint/no-unused-vars */
+export const ourFileRouter = {
+  imageUploader: f({
+    image: {
+      maxFileSize: '8MB',
+    },
+  })
+    .middleware(async ({ req }) => {
+      const user = await auth(req)
+
+      if (!user) throw new UploadThingError('Unauthorized')
+
+      return { userId: user.id }
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log('Upload complete for userId:', metadata.userId)
+
+      console.log('file url', file.ufsUrl)
+
+     return {
+       uploadedBy: metadata.userId,
+       url: file.url ?? file.ufsUrl ?? `https://utfs.io/f/${file.key}`,
+     }
+
+    }),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
