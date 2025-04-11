5904ddad533319bc76f878397bf6a70bb7246756
hwgit <hwgojj@gmail.com>
2025-04-11 15:38:24 -0700

    Reformat code for consistency

---
+++ src/components/ui/WrapForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const WrapForm = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='min-h-fit
+    h-full
+    flex
+    items-center
+    justify-center
+    pb-12
+    pt-6'>
+      <div className='
+      max-w-[600px]
+      w-full
+      flex-col
+      gap-6
+      items-center
+      shadow-xl
+      rounded-md
+      p-4
+      md:p-8
+      
+      '>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default WrapForm
---
+++ src/components/ui/toasts.tsx
@@ -0,0 +1,23 @@
+import { toast } from 'react-toastify'
+import { MdCheckCircle } from 'react-icons/md'
+
+
+export const showCustomToast = (message: string, bgColor = 'bg-green-500') => {
+  toast(
+     ({ closeToast }) => (
+      <div className={`flex items-center p-3 text-white rounded ${bgColor}`}>
+        <MdCheckCircle className="mr-2 text-white" size={20} />
+        <span>{message}</span>
+      </div>
+    ),
+    {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      closeButton: false,
+    }
+  )
+}
