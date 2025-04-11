e2b7b905373c2fdb4849310787f51911702c3d63
hwgit <hwgojj@gmail.com>
2025-04-11 15:28:22 -0700

    retrive data fro  database

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import ProductDetail from '../ProductDetail'
+
+const Page = () => {
+  const params = useParams()
+  const [product, setProduct] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      
+      if (!params || !params.id) {
+        setError('Invalid product ID')
+        setLoading(false)
+        return
+      }
+    console.log('Fetching product with ID1:', params.id)
+
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/product/${params.id}`) 
+        console.log('Fetching product with ID:', params.id)
+        console.log('Response:', res)
+        if (!res.ok) {
+          throw new Error('Failed to fetch product')
+        }
+                
+
+        const productData = await res.json()
+        setProduct(productData)
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message)
+        }
+        else {  
+          setError('An unknown error occurred')
+        }
+        
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchProduct()
+  }, [params?._id])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>
+  }
+
+  if (!product) {
+    return <div>Product not found</div>
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-11">
+      <ProductDetail product={product} />
+    </div>
+  )
+}
+
+export default Page
