0a0cb6f1f537f1d89faa6b3ec14e4a232e545586
hwgit <hwgojj@gmail.com>
2025-04-11 15:31:33 -0700

    initial commit

---
+++ src/components/input/CheckBox.tsx
@@ -0,0 +1,36 @@
+import { FieldValues, UseFormRegister } from "react-hook-form"
+
+interface CheckboxProps{
+    id:string
+    label:string
+    disabled?:boolean
+    required?:boolean
+    register:UseFormRegister<FieldValues>
+}
+
+const CheckBox: React.FC<CheckboxProps> = ({
+  id,
+  label,
+  disabled,
+  required ,
+  register,
+}) => {
+  return (
+    <div className="w-full flex flex-row gap-2 items-center">
+      <input
+        autoComplete="off"
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=""
+        type="checkbox"
+        className="cursor-pointer"
+      />
+      <label htmlFor={id} className="font-medium cursor-pointer">
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default CheckBox
---
+++ src/components/input/ColorSelect.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import React, { useCallback, useEffect, useState } from 'react'
+import { ImageType } from '../manager/addst' 
+import { UploadDropzone } from '@/hooks/utils/uploadthing' 
+import { toast } from '../ui/use-toast'
+
+import Button from '../ui/Button'
+
+interface ColorSelectProps {
+  item: ImageType
+  addImageToState: (value: ImageType) => void
+  RemoveImageFromState: (value: ImageType) => void
+  isProductCreated: boolean
+}
+
+const ColorSelect: React.FC<ColorSelectProps> = ({
+  item,
+  addImageToState,
+  RemoveImageFromState,
+  isProductCreated,
+}) => {
+  const [isSelected, setIsSelected] = useState(false)
+  const [selectedFile, setSelectedFile] = useState<ImageType | null>(null)
+  const [isUploading, setIsUploading] = useState(false)
+
+  // Reset file and selection when product is created
+  useEffect(() => {
+    if (isProductCreated) {
+      setIsSelected(false)
+      setSelectedFile(null)
+    }
+  }, [isProductCreated])
+
+  const handleCheck = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setIsSelected(e.target.checked)
+      if (!e.target.checked) {
+        setSelectedFile(null) 
+        RemoveImageFromState(item) 
+      }
+    },
+    [item, RemoveImageFromState]
+  )
+
+  const handleUploadComplete = (
+    res: { url: string; key: string; ufsUrl?: string }[]
+  ) => {
+    const uploadedFile = res[0]
+
+    const transformedImage: ImageType = {
+      ...item,
+      image: uploadedFile.ufsUrl ?? uploadedFile.url, 
+    }
+
+    setSelectedFile(transformedImage)
+    addImageToState(transformedImage)
+
+  }
+
+  const handleUploadError = (error: Error) => {
+    toast({ title: `ERROR! ${error.message}` })
+    setIsUploading(false)
+    setSelectedFile(null) 
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-1 overflow-y-auto border-b-[1.2px] border-slate-200 mb-2 p-2 gap-2 items-center">
+      <div className="flex flex-row items-center gap-2">
+        <input
+          type="checkbox"
+          id={item.color}
+          value={item.color}
+          checked={isSelected}
+          onChange={handleCheck}
+          className="cursor-pointer"
+        />
+        <label htmlFor={item.color} className="cursor-pointer">
+          {item.color}
+        </label>
+      </div>
+
+      {isSelected && (
+        <div className="col-span-2 text-center">
+          <UploadDropzone
+            className="p-2 border border-gray-600"
+            endpoint="imageUploader" // Adjust endpoint if needed
+            onClientUploadComplete={handleUploadComplete}
+            onUploadError={handleUploadError}
+          />
+        </div>
+      )}
+
+      {selectedFile && (
+        <div className="text-center mt-4">
+          {/* <Image
+            src={`${selectedFile.image}`}
+            alt="Uploaded preview"
+            width={100}
+            height={100}
+            className="max-w-xs"
+          /> */}
+          <Button
+            label="Remove"
+            onClick={() => {
+              setSelectedFile(null)
+              RemoveImageFromState(item) 
+            }}
+          />
+            
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ColorSelect
