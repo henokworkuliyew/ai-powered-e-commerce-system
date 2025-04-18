b82049735a7068eded1c2351970366a57ba83d17
hwgit <hwgojj@gmail.com>
2025-04-17 17:23:10 -0700

    add loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,45 @@
-import { ProductCard } from "@/components/ProductCard/productCard"
+'use client'
 
-import { Product } from "@/type/Product"
+import Loading from '@/components/Loading'
+import { ProductCard } from '@/components/ProductCard/productCard'
+import { Product } from '@/type/Product'
+import { useState, useEffect } from 'react'
 
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_APP_URL}/api/product`,
+          {
+            cache: 'no-store',
+          }
+        )
 
-export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/product`, {
-    cache: 'no-store', 
-  })
+        if (!res.ok) throw new Error('Failed to fetch')
 
-  if (!res.ok) {
-    
-    return <div className="text-red-500">Failed to load products.</div>
-  }
-  
-  const data = await res.json()
-  
-  const products: Product[] = data.products || []
+        const data = await res.json()
+        setProducts(data.products || [])
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false)
+      }
+    }
 
+    fetchProducts()
+  }, [])
+
+  if (loading) return <Loading />
 
   return (
-    <div className="grid grid-cols-2 gap-8 w-full  sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-      {products?.map((product, index: number) => {
-
-        return <ProductCard key={index} product = {product} />
-
-      })}
+    <div className="grid grid-cols-2 gap-8 w-full sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
     </div>
   )
 }
