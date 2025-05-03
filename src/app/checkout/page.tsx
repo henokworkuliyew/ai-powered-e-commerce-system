08ef939340609c8f802bd7c36c2a31084f9725e5
henokworkuliyew <hw93gojj@gmail.com>
2025-05-02 22:17:50 -0700

    authorze

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,16 @@
 //import WrapForm from '@/components/ui/WrapForm'
 import { Toaster } from '@/components/ui/toaster'
 import Checkout from './Checkout'
+import { getCurrentUser } from '@/action/CurrentUser'
+import UnauthorizedPage from '@/components/unauthorized'
 
-const CheckoutPage = () => {
+const CheckoutPage = async () => {
+  const currentUser = await getCurrentUser()
+  if(!currentUser) {
+    return <div>
+      <UnauthorizedPage/>
+    </div>
+  }
   return (
     <div className="p-8 bg-slate-100">
          <Toaster/>
