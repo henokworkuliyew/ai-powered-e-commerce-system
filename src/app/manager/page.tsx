4d4bf1ee3afc6b303ba774a61784963fe011682e
henokworkuliyew <hw93gojj@gmail.com>
2025-05-05 13:58:33 -0700

    Add authentication check to Manager page, restricting access to users with MANAGER role and displaying NullData component for unauthorized users.

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,12 +1,14 @@
-
+import { getCurrentUser } from '@/action/CurrentUser'
+import NullData from '@/components/NullData'
 import Manager from './Manager'
 
-const page = () => {
-  return (
-    <div>
-      <Manager/>
-    </div>
-  )
+
+const page = async () => {
+  const currentUser = await getCurrentUser()
+  if (!currentUser || currentUser.role !== 'MANAGER')
+    return <NullData title="Access denied!" />
+
+  return <Manager />
 }
 
 export default page
