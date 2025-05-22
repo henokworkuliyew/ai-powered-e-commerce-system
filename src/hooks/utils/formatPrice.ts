38e4aa0ad377ba9195ad7abe57ae779600fe7beb
henokworkuliyew <hw93gojj@gmail.com>
2025-05-22 20:30:44 +0300

    change format price

--- src/hooks/utils/formatPrice.ts
+++ src/hooks/utils/formatPrice.ts
@@ -1,5 +1,5 @@
 export const FormatPrice = (amount: number) => {
-  const Amount = amount*100;
+  const Amount = amount;
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'ETB',
