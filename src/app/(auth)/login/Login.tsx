c0b62cad5ff68c5ef8e400f344ff0113fbf1e305
henokworkuliyew <hw93gojj@gmail.com>
2025-05-22 07:45:49 +0300

    fix deploy error

--- src/app/(auth)/login/Login.tsx
+++ src/app/(auth)/login/Login.tsx
@@ -69,17 +69,7 @@ const Login: React.FC<LoginProps> = ({ currentUser }) => {
         register={register}
         errors={errors}
         required
-        validation={{
-          required: 'Email is required',
-          pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message: 'Please enter a valid email address',
-          },
-          maxLength: {
-            value: 100,
-            message: 'Email must be less than 100 characters',
-          },
-        }}
+        
       />
 
       <Input
@@ -90,23 +80,7 @@ const Login: React.FC<LoginProps> = ({ currentUser }) => {
         errors={errors}
         required
         type="password"
-        validation={{
-          required: 'Password is required',
-          minLength: {
-            value: 8,
-            message: 'Password must be at least 8 characters',
-          },
-          maxLength: {
-            value: 50,
-            message: 'Password must be less than 50 characters',
-          },
-          pattern: {
-            value:
-              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            message:
-              'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-          },
-        }}
+       
         showPasswordToggle
       />
 
