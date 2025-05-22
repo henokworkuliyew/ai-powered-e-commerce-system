de249bafa4f431bb79d4d963e314dba64953b2d7
henokworkuliyew <hw93gojj@gmail.com>
2025-05-22 07:47:48 +0300

    add toaster

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,25 +1,26 @@
-
-import AdminNav from "@/components/admin/AdminNav"
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'AI-powered e-commerce admin page',
-  keywords: ['e-commerce', 'AI', 'shopping', 'online store'],
- 
-}
+import type React from 'react'
+// import { redirect } from 'next/navigation'
+import { getCurrentUser } from '@/action/CurrentUser'
+import AdminSidebar from '@/components/admin/adminSideBar'
+import { Toaster } from '@/components/ui/toaster'
+// import { AdminSidebar } from '@/components/admin/adminSideBar'
 
 export default async function AdminLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    
+  }
+
   return (
-    <div>
-      <AdminNav/>
-      <div>
-        {children}
-      </div>
+    <div className="flex min-h-screen">
+      <Toaster />
+      <AdminSidebar />
+      <main className="flex-1 p-6 bg-gray-50 ">{children}</main>
     </div>
   )
 }
