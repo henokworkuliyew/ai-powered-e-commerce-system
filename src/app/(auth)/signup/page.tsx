f1216b5bbbb8b83a61a20a8be9875f0163c923cf
henokworkuliyew <hw93gojj@gmail.com>
2025-05-25 21:51:13 +0300

    update

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Register from '../register/Register'
+import Register from './RegisterPage'
 import { getCurrentUser } from '@/action/CurrentUser'
 
 const page = async () => {
