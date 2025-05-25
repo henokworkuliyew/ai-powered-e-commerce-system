443a9e9a33b5c4cb46990971bddc6c46b20751d2
henokworkuliyew <hw93gojj@gmail.com>
2025-05-25 14:28:14 +0300

    remove register page wraping with wraper

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,4 @@
 
-import WrapForm from '@/components/ui/WrapForm'
-
 import React from 'react'
 import Register from './Register'
 import { getCurrentUser } from '@/action/CurrentUser'
@@ -8,9 +6,9 @@ import { getCurrentUser } from '@/action/CurrentUser'
 const page = async () => {
   const currentUser = await getCurrentUser()
   return (
-    <WrapForm>
+    
       <Register currentUser={currentUser} />
-    </WrapForm>
+    
   )
 }
 
