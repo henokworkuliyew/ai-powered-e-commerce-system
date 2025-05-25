bc051b31322b22531d5c4bfee5e383ecea80d01b
henokworkuliyew <hw93gojj@gmail.com>
2025-05-25 14:27:53 +0300

    remove login page wraping with wraper

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Login from './Login'
 
-import WrapForm from '@/components/ui/WrapForm'
-
 import { getCurrentUser } from '@/action/CurrentUser'
 
 const page = async () => {
   const currentUser = await getCurrentUser()
   return (
-    <WrapForm>
+ 
       <Login currentUser={currentUser} />
-    </WrapForm>
+
   )
 }
 
