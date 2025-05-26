8d8f079a177569ceddb09daa95fc3e2e1ce185ff
henokworkuliyew <hw93gojj@gmail.com>
2025-05-26 05:42:07 +0300

    notfound page

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'
 
 export default function NotFound() {
-  return <div>Custom 404 Page</div>;
+  return <div>Custom 404 Page</div>
 }
