83a1302ac78fade0c153e2d6ea2af9b3b066b2f3
henokworkuliyew <hw93gojj@gmail.com>
2025-05-26 07:03:01 +0300

    update the notfound page

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+// 'use client'
 
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
+// import { useSearchParams } from 'next/navigation'
+// import { Suspense } from 'react'
 
-export default function NotFound() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <NotFoundContent />
-    </Suspense>
-  )
-}
+// export default function NotFound() {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <NotFoundContent />
+//     </Suspense>
+//   )
+// }
 
-function NotFoundContent() {
-  const searchParams = useSearchParams()
-  return <div>404 with params: {searchParams?.get('error')}</div>
-}
+// function NotFoundContent() {
+//   const searchParams = useSearchParams()
+//   return <div>404 with params: {searchParams?.get('error')}</div>
+// }
