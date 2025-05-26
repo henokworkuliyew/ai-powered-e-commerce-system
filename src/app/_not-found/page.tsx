9119af7c52eb9931f26c48aa83e56c466d05b3aa
henokworkuliyew <hw93gojj@gmail.com>
2025-05-26 05:43:22 +0300

    padate ot-found page

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,5 +1,17 @@
-export const dynamic = 'force-dynamic'
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function NotFound() {
-  return <div>Custom 404 Page</div>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NotFoundContent />
+    </Suspense>
+  )
+}
+
+function NotFoundContent() {
+  const searchParams = useSearchParams()
+  return <div>404 with params: {searchParams?.get('error')}</div>
 }
