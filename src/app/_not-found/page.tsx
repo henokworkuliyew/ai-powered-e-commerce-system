33e510f27cff3cd13755117131dae10c4509eb4b
henokworkuliyew <hw93gojj@gmail.com>
2025-05-26 07:24:12 +0300

    update nofoud page

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,17 +1,3 @@
-// 'use client'
-
-// import { useSearchParams } from 'next/navigation'
-// import { Suspense } from 'react'
-
-// export default function NotFound() {
-//   return (
-//     <Suspense fallback={<div>Loading...</div>}>
-//       <NotFoundContent />
-//     </Suspense>
-//   )
-// }
-
-// function NotFoundContent() {
-//   const searchParams = useSearchParams()
-//   return <div>404 with params: {searchParams?.get('error')}</div>
-// }
+export default function NotFound() {
+  return <h1>404 - Page Not Found</h1>
+}
