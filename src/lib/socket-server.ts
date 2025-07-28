f98207b661d42105e3efadadb0aa0688728ded69
henokworkuliyew <hw93gojj@gmail.com>
2025-07-28 12:48:45 +0300

    fix: update the url to NEXTAUTH_URL

--- src/lib/socket-server.ts
+++ src/lib/socket-server.ts
@@ -20,7 +20,7 @@ export const initializeSocket = (server: HTTPServer) => {
       cors: {
         origin:
           process.env.NODE_ENV === 'production'
-            ? process.env.NEXT_PUBLIC_APP_URL
+            ? process.env.NEXTAUTH_URL
             : ['http://localhost:3000', 'http://localhost:3001'],
         methods: ['GET', 'POST'],
         credentials: true,
